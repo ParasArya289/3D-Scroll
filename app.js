86e7a396f72ca096306963df9804de4ddc9589c5
Paras Arya <parasarya289@gmail.com>
2023-02-03 22:16:03 +0530

    style: loading

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ canvas.height = window.innerHeight;
 
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
-        // document.querySelector("body").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
     } else {
         document.querySelector("#loader").style.display = "none";
-        // document.querySelector("body").style.visibility = "visible";
     }
 };
 

a0ea252eb43fafcf1cf28a1cc819e34f66fd32b2
Paras Arya <parasarya289@gmail.com>
2023-02-09 22:23:45 +0530

    style: redesign loader

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ canvas.height = window.innerHeight;
 
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden"
         document.querySelector("#loader").style.visibility = "visible";
     } else {
+        document.querySelector("body").style.visibility = "visible"
         document.querySelector("#loader").style.display = "none";
     }
 };
